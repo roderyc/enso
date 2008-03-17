90f7d2bf447f816737a1bfb63a0f5f46f3413678
ebjono <ebjono@8b7adc99-b347-0410-ae0a-d9e86c8d69b5>
2008-03-17 18:28:22 +0000

    Created a "graphics" directory in enso_win32 where the graphics backend object code will go when compiled.
    git-svn-id: http://enso.googlecode.com/svn/trunk@57 8b7adc99-b347-0410-ae0a-d9e86c8d69b5

---
+++ win32/enso_win32/graphics/__init__.py
@@ -0,0 +1,5 @@
+# A placeholder.  Must provide TransparentWindow class and
+# getDesktopSize() method.
+
+from TransparentWindow import TransparentWindow
+from TransparentWindow import _getDesktopSize as getDesktopSize
