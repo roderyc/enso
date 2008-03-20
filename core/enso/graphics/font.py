32b7eea58782006f4e7d674ed47e4b20680c15fd
ebjono <ebjono@8b7adc99-b347-0410-ae0a-d9e86c8d69b5>
2008-03-20 00:19:40 +0000

    Hmm, I think my previous commit, where I did a HORRIBLE HACK to core/enso/graphics/font.py in order to make Enso run on Windows, accidentally broke Enso on the other platforms; I forgot that I was modifying core.  To fix this for now, I put some code around it which detects the current OS and does the horrible hack only if it's windows.
    git-svn-id: http://enso.googlecode.com/svn/trunk@88 8b7adc99-b347-0410-ae0a-d9e86c8d69b5

--- core/enso/graphics/font.py
+++ core/enso/graphics/font.py
@@ -132,14 +132,22 @@ class Font:
         """
 
 
-        # TODO THE FOLLOWING LINE IS A HORRIBLE HACK AND MUST BE FIXED
-        # ASAP:
-        cairoContext.select_font_face(
-            "c:/WINDOWS/Fonts/arial.ttf",
-            # self.name,
-            self.slant,
-            cairo.FONT_WEIGHT_NORMAL
-            )
+        # TODO THE WINDOWS SPECIAL CASE WITH THE HARD-CODED PATH TO ARIAL
+        # IS A HORRIBLE HACK AND MUST BE FIXED ASAP (besides, Arial is
+        # ugly).
+        import sys
+        if sys.platform.startswith( "win" ):
+            cairoContext.select_font_face(
+                "c:/WINDOWS/Fonts/arial.ttf",
+                self.slant,
+                cairo.FONT_WEIGHT_NORMAL
+                )
+        else:
+            cairoContext.select_font_face(
+                self.name,
+                self.slant,
+                cairo.FONT_WEIGHT_NORMAL
+                )
 
         cairoContext.set_font_size( self.size )
 
